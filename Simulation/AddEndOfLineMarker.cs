85914b7f68b05d27dfd9a35eee0188e96421f749
KnightsUCF <peterk@knights.ucf.edu>
2021-03-14 20:03:09 -0400

    Update AddEndOfLineMarker.cs

--- Simulation/AddEndOfLineMarker.cs
+++ Simulation/AddEndOfLineMarker.cs
@@ -23,19 +23,19 @@ public class AddEndOfLineMarker : MonoBehaviour
     }
 
 
-    public void Run()
-    {
-        headingText.text = "ADDING END OF LINE MARKER (X)";
-        cam.ZoomOut();
-        AddFlashingCubeToEnd();
-    }
-
-
     void AddFlashingCubeToEnd()
     {
         Vector3 pos = sequenceList.GetEndOfLineFlashingCubePosition();
         GameObject marker = Instantiate(flashingCube, pos, Quaternion.identity, this.transform);
         sequenceList.AddToList(marker);
+    }
+    
+    
+    public void Run()
+    {
+        headingText.text = "ADDING END OF LINE MARKER (X)";
+        cam.ZoomOut();
+        AddFlashingCubeToEnd();
     }
 
 }
